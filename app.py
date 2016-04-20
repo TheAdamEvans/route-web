c6cc78c5e34ef9aaac87982933d3a1700c85312a
TheAdamEvans <theadamevans@gmail.com>
2016-04-20 00:41:00 -0600

    turned debugger off

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # Load default config and override config from an environment variable
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'climbon.db'),
-    DEBUG=True,
+    DEBUG=False,
     SECRET_KEY='cookie'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
